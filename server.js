8c996455a358654dfc2d2bb3f59aaac21683e1cd
Joaquin Morales Gastaldo <401726@sistemas.frc.utn.edu.ar>
2026-07-02 14:51:02 -0300

    Add runtime health and version endpoints

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
+const APP_VERSION = process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'local';
 
 // --- CONFIG ---
 const PORT = process.env.PORT || 3000;
@@ -27,11 +28,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get(/^(?!\/api).*/, (req, res) => {
+app.get('/healthz', (req, res) => {
+    res.json({ ok: true, version: APP_VERSION });
+});
+
+app.get('/version', (req, res) => {
+    res.json({ version: APP_VERSION });
+});
+
+app.get(/^(?!\/api|\/healthz|\/version).*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
